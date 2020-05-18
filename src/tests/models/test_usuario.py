a8810e25c8509ffa9d0d30144eb95036534ae01b
Franco Liberali <franco.liberali@gmail.com>
2020-05-18 00:31:48 -0300

    usar crear usuario del base test

--- src/tests/models/test_usuario.py
+++ src/tests/models/test_usuario.py
@@ -1,18 +1,11 @@
 import unittest
 
-from app import db
 from app.models.usuario import Usuario
 from tests.base import BaseTestCase
 
 class UsuarioTestCase(BaseTestCase):
     def test_generar_auth_token(self):
-        usuario = Usuario(
-            email='test@test.com',
-            password='test'
-        )
-        # TODO UsuarioRepositorio
-        db.session.add(usuario)
-        db.session.commit()
+        usuario = self.crear_usuario('test@test.com', 'test')
         auth_token = usuario.generar_auth_token()
         self.assertTrue(isinstance(auth_token, bytes))
         self.assertTrue(Usuario.validar_auth_token(auth_token) == usuario.id)
