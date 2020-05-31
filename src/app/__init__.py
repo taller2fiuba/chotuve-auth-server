566f7c9ce161b6b424870d88162fed263a6c0511
edjus <ejusto@fi.uba.ar>
2020-05-31 19:35:33 -0300

    Cambio endpoint para obtener usuarios

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -17,8 +17,8 @@ API = Api(app)
 from .resources import ping, usuario, usuarios, sesion, base_de_datos
 
 API.add_resource(ping.PingResource, '/ping')
-API.add_resource(usuario.UsuarioResource, '/usuario')
-API.add_resource(usuarios.UsuariosResource, '/usuarios', endpoint='Usuarios')
+API.add_resource(usuario.UsuarioResource, '/usuario', methods=["POST"])
+API.add_resource(usuarios.UsuariosResource, '/usuario', methods=["GET"], endpoint='Usuarios')
 API.add_resource(usuario.UsuarioResource, '/usuario/<int:usuario_id>', methods=["GET"], endpoint='UsuarioConIdResource')
 API.add_resource(sesion.SesionResource, '/usuario/sesion')
 API.add_resource(base_de_datos.BaseDeDatosResource, '/base_de_datos')
