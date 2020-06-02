ec4012875ca2010c334e8d1852e09759e6f042fa
edjus <ejusto@fi.uba.ar>
2020-06-02 01:55:57 -0300

    obtener usuario por id devuelve datos completos

--- src/app/resources/usuario_id.py
+++ src/app/resources/usuario_id.py
@@ -8,6 +8,6 @@ class UsuarioIdResource(Resource):
     def get(self, usuario_id):
         try:
             usuario = Usuario.query.filter_by(id=usuario_id).one()
-            return {'email': usuario.email}, 200
+            return usuario.serializar(), 200
         except flask_sqlalchemy.orm.exc.NoResultFound:
             return {}, 404
