7fdae4a2d00ab4f6dc948d8b5e686fba1d814ca2
Lucho <sportelliluciano@gmail.com>
2020-06-06 21:32:02 -0300

    Agrego logger y SEMVER

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -5,7 +5,8 @@ import requests
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-from config import Config
+from config import Config, configurar_logger
+import logging
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 api = Api(app)
+
+configurar_logger()
+log = logging.getLogger(__name__)
 
 from .resources import ping, usuario, usuario_id, sesion, base_de_datos
 
@@ -22,5 +26,13 @@ api.add_resource(usuario_id.UsuarioIdResource, '/usuario/<int:usuario_id>')
 api.add_resource(sesion.SesionResource, '/usuario/sesion')
 api.add_resource(base_de_datos.BaseDeDatosResource, '/base_de_datos')
 
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    tb = traceback.format_exc()
+    log.warn(f'Excepcion no manejada: {tb}')
+    return {'mensaje': str(e)}, 500
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+log.info(f'Iniciando version de la app: {Config.APP_VERSION}')
