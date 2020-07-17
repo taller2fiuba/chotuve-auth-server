0ab1917e8e16e295af519884ba89febe05ecf881
Lucho <sportelliluciano@gmail.com>
2020-07-17 13:35:24 -0300

    Actualizo la documentación

--- src/app/resources/usuario_id.py
+++ src/app/resources/usuario_id.py
@@ -34,7 +34,7 @@ class UsuarioIdResource(Resource):
             usuario.foto = post_data['foto']
         if 'habilitado' in post_data:
             if not g.es_admin:
-                return {'mensaje': 'Requiere admin'}, 401
+                return {'mensaje': 'Requiere admin'}, 403
             usuario.habilitado = bool(post_data['habilitado'])
 
         db.session.commit()
